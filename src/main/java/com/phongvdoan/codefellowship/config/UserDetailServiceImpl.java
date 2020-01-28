91cd4b58bd7512a79f2b348bffff4b5bc1557256
Phong <phongdoan.dev@gmail.com>
2020-01-28 10:46:27 -0800

    added java files for spring

---
+++ src/main/java/com/phongvdoan/codefellowship/config/UserDetailServiceImpl.java
@@ -0,0 +1,21 @@
+package com.phongvdoan.codefellowship.config;
+
+import com.phongvdoan.codefellowship.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserRepository.findByUsername(username);
+    }
+}
+
